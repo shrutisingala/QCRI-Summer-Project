ec89667481992c3b559f1b700dab4fa7ff5df644
shrutisingala <ssingala@gmail.com>
2016-05-18 15:06:29 +0300

    added AlertManager

--- src/org/qcri/aidr/alerts/items/AlertManager.java
+++ src/org/qcri/aidr/alerts/items/AlertManager.java
@@ -5,6 +5,7 @@
  */
 package org.qcri.aidr.alerts.items;
 
+import org.qcri.aidr.alerts.*;
 import java.util.List;
 
 /**
@@ -12,9 +13,24 @@ import java.util.List;
  * @author shrutisingala
  */
 public class AlertManager {
-    
-    public List<AlertMessage> getAlerts(List<AlertMessage> entries) {
-    return entries;
+
+    public static List<AlertMessage> getAlerts() {
+
+        RSSFeedParser parser = new RSSFeedParser("http://www.gdacs.org/xml/rss.xml");
+        Alerts alert = parser.readAlert();
+
+        String publish_time = alert.alertTime.toString();
+        SaveFile sf = new SaveFile("http://www.gdacs.org/xml/rss.xml", publish_time);
+
+        return alert.entries;
+
     }
-    
+
+    public static void main(String args[]) {
+        List<AlertMessage> ListOfAlerts = getAlerts();
+
+        String publish_time = ListOfAlerts.toString();
+        System.out.println(publish_time);
+    }
+
 }
