9a6a2fc0686b1b7899a695fdc2c0f54c4920b316
shrutisingala <ssingala@gmail.com>
2016-05-20 17:03:23 +0300

    connected sql with some warnings

--- src/org/qcri/aidr/alerts/sql/DBManager.java
+++ src/org/qcri/aidr/alerts/sql/DBManager.java
@@ -2,148 +2,150 @@ package org.qcri.aidr.alerts.sql;
 
 import org.qcri.aidr.alerts.items.*;
 import java.sql.*;
-import java.io.*; 
+import java.io.*;
 import java.lang.*;
 
-public class DBManager { 
+public class DBManager {
 
 //import mysql-connector-java-5.1.32.jar 
 //http://www.tutorialspoint.com/jdbc/jdbc-create-tables.htm 
-final private static String JDBC_URL = "jdbc:mysql://localhost:3306/mudra"; 
-final private static String USER = "root"; 
-final private static String PASSWORD = "salvivado123"; 
+    final private static String JDBC_URL = "jdbc:mysql://localhost:3306/mudra";
+    final private static String USER = "root";
+    final private static String PASSWORD = "shruti";
 
-public static void createalerts3(int alert_id, String alert_type, String alert_time, String alert_severity, String alert_population){
-             Connection connection = null;
-             PreparedStatement preparedStatement = null;
-             try {
-                   // Class.forName(JDBC_DRIVER);
-                    connection = DriverManager.getConnection(JDBC_URL, USER,PASSWORD);
-                    preparedStatement = connection.prepareStatement("INSERT INTO alerts3 VALUES(?,?,?,?,?)");
-                    preparedStatement.setFloat(1,alert_id);
-                    preparedStatement.setString(2,alert_type);
-                    preparedStatement.setString(3,alert_time);
-                    preparedStatement.setString(4,alert_severity);
-                    preparedStatement.setString(5,alert_population);
-                    boolean b=preparedStatement.execute();
-                    if(b==true)
-                           System.out.println("1 record inserted...");
-             } catch (SQLException sqlEx) {
-                   // sqlEx.printStackTrace();
-                    System.out.println("hey there");
-                    System.exit(1);
-             } /*catch (ClassNotFoundException clsNotFoundEx) {
+    public static void createmaster_alerts(int alert_id, String alert_type, String alert_time, String alert_severity, String alert_population) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            // Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+            preparedStatement = connection.prepareStatement("INSERT INTO master_alerts VALUES(?,?,?,?,?)");
+            preparedStatement.setFloat(1, alert_id);
+            preparedStatement.setString(2, alert_type);
+            preparedStatement.setString(3, alert_time);
+            preparedStatement.setString(4, alert_severity);
+            preparedStatement.setString(5, alert_population);
+            boolean b = preparedStatement.execute();
+            if (b == true) {
+                System.out.println("1 record inserted...");
+            }
+        } catch (SQLException sqlEx) {
+            //sqlEx.printStackTrace();
+            System.out.println("hey there");
+            System.exit(1);
+        } /*catch (ClassNotFoundException clsNotFoundEx) {
                     clsNotFoundEx.printStackTrace();
                     System.exit(1);
              }*/ finally {
-                    try {
-                           preparedStatement.close();
-                           connection.close();
-                    } catch (Exception e) {
-                           System.exit(1);
+            try {
+                preparedStatement.close();
+                connection.close();
+            } catch (Exception e) {
+                System.exit(1);
+            }
+        }
+    }
+
+    public static void updatetype(int alert_id, String alert_type) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            // Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+            preparedStatement = connection.prepareStatement("UPDATE master_alerts SET alert_type=? WHERE alert_id=?");
+            preparedStatement.setString(1, alert_type);
+            preparedStatement.setInt(2, alert_id);
+            boolean b = preparedStatement.execute();
+            if (b == true) {
+                System.out.println("alert type changed for alert id " + alert_id);
+            }
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+            System.exit(1);
+        } finally {
+            try {
+                preparedStatement.close();
+                connection.close();
+            } catch (Exception e) {
+                System.exit(1);
+            }
+        }
+    }
+
+    public static void readalert() {
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            // Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(JDBC_URL, USER,
+                    PASSWORD);
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM master_alerts");
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int noCols = metaData.getColumnCount();
+            for (int i = 1; i <= noCols; i++) {
+                if (i != 3) {
+                    System.out.printf("%-10s\t", metaData.getColumnName(i).toUpperCase());
+                }
+            }
+            System.out.println();
+            while (resultSet.next()) {
+                for (int i = 1; i <= noCols; i++) {
+                    if (i != 3) {
+                        System.out.printf("%-10s\t", resultSet.getObject(i));
                     }
-             }
-       }
+                }
+                System.out.println();
+            }
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+            System.exit(1);
+        } finally {
+            try {
+                statement.close();
+                connection.close();
+            } catch (Exception e) {
+                System.exit(1);
+            }
+        }
+    }
+
+    public static void deletealert(int alert_id) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            //Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+            preparedStatement = connection.prepareStatement("DELETE FROM master_alerts WHERE alert_id=?");
+            preparedStatement.setFloat(1, alert_id);
+            boolean b = preparedStatement.execute();
+            if (b == true) {
+                System.out.println("1 record deleted...");
+            }
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+            System.exit(1);
+        } finally {
+            try {
+                preparedStatement.close();
+                connection.close();
+            } catch (Exception e) {
+                System.exit(1);
+            }
+        }
+    }
 
 
-public static void updatetype(int alert_id, String alert_type){
-             Connection connection = null;
-             PreparedStatement preparedStatement = null;
-             try {
-                   // Class.forName(JDBC_DRIVER);
-                    connection = DriverManager.getConnection(JDBC_URL, USER,PASSWORD);
-                    preparedStatement = connection.prepareStatement("UPDATE alerts3 SET alert_type=? WHERE alert_id=?");
-                    preparedStatement.setString(1, alert_type);                   
-                    preparedStatement.setInt(2, alert_id);
-                    boolean b=preparedStatement.execute();
-                    if(b==true)
-                           System.out.println("alert type changed for alert id "+alert_id);
-             } catch (SQLException sqlEx) {
-                    sqlEx.printStackTrace();
-                    System.exit(1);
-             } finally {
-                    try {
-                           preparedStatement.close();
-                           connection.close();
-                    } catch (Exception e) {
-                           System.exit(1);
-                    }
-             }
-       }
-
-
-       public static void readalert() {
-             Connection connection = null;
-             Statement statement = null;
-             try {
-                   // Class.forName(JDBC_DRIVER);
-                    connection = DriverManager.getConnection(JDBC_URL, USER,
-                                  PASSWORD);
-                    statement = connection.createStatement();
-                    ResultSet resultSet = statement.executeQuery("SELECT * FROM alerts3");
-                    ResultSetMetaData metaData = resultSet.getMetaData();
-                    int noCols = metaData.getColumnCount();
-                    for (int i = 1; i <= noCols; i++) {
-                           if (i != 3)
-                                  System.out.printf("%-10s\t", metaData.getColumnName(i).toUpperCase());
-                    }
-                    System.out.println();
-                    while (resultSet.next()) {
-                           for (int i = 1; i <= noCols; i++) {
-                                  if (i != 3)
-                                        System.out.printf("%-10s\t", resultSet.getObject(i));
-                           }
-                           System.out.println();
-                    }
-             } catch (SQLException sqlEx) {
-                    sqlEx.printStackTrace();
-                    System.exit(1);
-             } finally {
-                    try {
-                           statement.close();
-                           connection.close();
-                    } catch (Exception e) {
-                           System.exit(1);
-                    }
-             }
-       }
-       
-       
-              public static void deletealert(int alert_id){
-             Connection connection = null;
-             PreparedStatement preparedStatement = null;
-             try {
-                    //Class.forName(JDBC_DRIVER);
-                    connection = DriverManager.getConnection(JDBC_URL, USER,PASSWORD);
-                    preparedStatement = connection.prepareStatement("DELETE FROM alerts3 WHERE alert_id=?");
-                    preparedStatement.setFloat(1, alert_id);               
-                    boolean b=preparedStatement.execute();
-                    if(b==true)
-                           System.out.println("1 record deleted...");
-             } catch (SQLException sqlEx) {
-                    sqlEx.printStackTrace();
-                    System.exit(1);
-             } finally {
-                    try {
-                           preparedStatement.close();
-                           connection.close();
-                    } catch (Exception e) {
-                           System.exit(1);
-                    }
-             }
-       }
-
-
-/*public static void main(String[] args) throws IOException { 
+    /*public static void main(String[] args) throws IOException { 
     
-             //createalerts3(1, "abc","hello", "hi", "New Jersey");
-             //createalerts3(2, "Jones", "hshdd", "djbjd", "Jersey");
-             //createalerts3(3, "Kapil","ejdgdi","jdhjdh", "Los Angeles");
+             //createmaster_alerts(1, "abc","hello", "hi", "New Jersey");
+             //createmaster_alerts(2, "Jones", "hshdd", "djbjd", "Jersey");
+             //createmaster_alerts(3, "Kapil","ejdgdi","jdhjdh", "Los Angeles");
              updatetype(1,"pqr");
              updatetype(2,"heyyaa");
              deletealert(2);
              readalert();
-             createalerts3(2, "Jones", "hshdd", "djbjd", "Jersey");
+             createmaster_alerts(2, "Jones", "hshdd", "djbjd", "Jersey");
              readalert();
 
         
@@ -152,7 +154,7 @@ public static void updatetype(int alert_id, String alert_type){
 Statement statement = null; 
 PreparedStatement preparedStatement = null; */
 
-/*try{ 
+ /*try{ 
 //CONNECTING TO THE DATABASE 
 myConnection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD); 
 
@@ -163,7 +165,7 @@ System.out.println("Connected to database"); //CONNECTED
 System.out.println("Creating table in given database..."); 
 statement = myConnection.createStatement(); 
 
-String sql = "CREATE TABLE alerts3 " + 
+String sql = "CREATE TABLE master_alerts " + 
 "(alert_id FLOAT not NULL, " + 
 " alert_type VARCHAR(50), " + 
 " alert_time VARCHAR(50), " + 
@@ -177,7 +179,7 @@ System.out.println("Created table in given database...");
 
 
 //CREATING A PREPARED STATEMENT 
-String insertTableSQL = "INSERT INTO alerts3" 
+String insertTableSQL = "INSERT INTO master_alerts" 
 + "(alert_id, alert_type, alert_time, alert_severity, alert_population) VALUES" 
 + "(?,?,?,?, ?)"; 
 
@@ -248,8 +250,6 @@ myConnection.close();
 } 
 System.out.println("Connection closed"); 
 } */
-
-
 } 
 
         
